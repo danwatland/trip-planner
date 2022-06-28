990922a19ce597e7c2d3a7816ff057456de27fdb
Dan Watland <danwatland@gmail.com>
2022-06-27 20:06:31 -0500

    Getting map working

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,40 @@
-import { useState } from 'react'
+import * as React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {GoogleMap, useJsApiLoader} from "@react-google-maps/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [map, setMap] = React.useState(null);
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: 'AIzaSyChtD7AbqlxkkUm-Zc_ly8VdjpO0Vcz3Ss'
+    });
+
+    const onLoad = React.useCallback((map) => {
+        setMap(map)
+    }, []);
+
+    const onUnmount = React.useCallback(() => {
+        setMap(null)
+    }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {isLoaded && <GoogleMap
+          center={{
+              lat: 41.6535,
+              lng: -93.709
+          }}
+          mapContainerStyle={{ width: '800px', height: '600px' }}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+          zoom={11}
+      >
+
+      </GoogleMap>}
     </div>
   )
 }
 
-export default App
+export default React.memo(App);
