936f3ab4295e9481057d8eb3c057f39a1c6865a2
Dan Watland <danwatland@gmail.com>
2022-07-02 22:22:21 -0500

    Updating build files

--- .aws/scripts/after_install.sh
+++ .aws/scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/trip-planner-ui
 npm install
+npm run build
 npm install pm2 -g
--- .aws/scripts/app_start.sh
+++ .aws/scripts/app_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/trip-planner-ui
-pwd
 npm start
 pm2 start npm --name "trip-planner-ui" -- start
 pm2 startup
